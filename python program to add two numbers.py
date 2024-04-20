c12056d245b6cc4d85d256201eea0cab50b5c133
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-04-20 21:59:24 +0530

    Add files via upload

---
+++ python program to add two numbers.py
@@ -0,0 +1,9 @@
+#python program to add two numbers
+a = float(input("Enter first number :"))
+b = float(input("Enter second number :"))
+
+#to add two numbers
+sum = a + b
+
+#printing values
+print("Sum of", a , "and", b , "is",sum)
