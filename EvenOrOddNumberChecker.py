1a268922ca92f0f65dbd8fd162034ecab83a3fbd
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-11-21 11:11:53 +0530

    Add files via upload

---
+++ EvenOrOddNumberChecker.py
@@ -0,0 +1,10 @@
+# Even or Odd Number Checker
+
+# Input: Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
